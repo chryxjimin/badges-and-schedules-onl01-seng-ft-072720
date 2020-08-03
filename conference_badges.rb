c96f9c7553e2e3bbd99d0ddd29cb63e316b840bb
Christie Yoon <cleex1999@gmail.com>
2020-08-03 04:21:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
   end
 
-#def batch_badge_creator(speakers)
 def batch_badge_creator(names)
   badge_array = []
   names.each do |name|
     badge_array << badge_maker(name)
-    #puts "Hello, my name is #{name}."
   end
   badge_array
 end
@@ -21,16 +19,10 @@ def assign_rooms(room_assignments)
   end
 
   def printer(intro)
-    #new_array = []
-    #new_array_2 = []
     batch_badge_creator(intro).each do |batch|
       puts batch
     end
     assign_rooms(intro).each do |room|
       puts room
-         #puts "Hello, my name is #{name}."
-         #puts "Hello, #{speaker}! You'll be assigned to room #{room_number + 1}!"
-         #return new_array
-         #return new_array_2
   end
 end
